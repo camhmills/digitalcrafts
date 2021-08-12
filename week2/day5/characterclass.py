d8f42afc4d11f8e894ac9c0a4d088fdfc7903370
Cameron Mills <camhmills@gmail.com>
2021-08-12 15:27:46 -0400

    Fixes a small bug where I forgot to import random into the class.

--- week2/day5/characterclass.py
+++ week2/day5/characterclass.py
@@ -1,11 +1,22 @@
+import random
 class Character:
-    def __init__(self, name, stamina, constitution, strength):
+    def __init__(self, name, stamina, constitution, strength, money):
         self.name = name
-        self.consitution = constitution
+        self.constitution = constitution
         self.stamina = stamina
         self.strength = strength
         self.inventory = []
-    
+        self.money = money
+
+    def acquireItems(self, item, itemBoost, itemTitle):
+        self.item = item
+        itemBoost = random.randint(2,5)
+        item = {"Sword" : itemBoost}
+        self.inventory.append(item)
+
+    def addMoney(self):
+        self.money += 1
+
     def damageCalc(self, damage):
         damage = self.strength * 1.5 +- random.randint(2,5)
         self.stamina -= damage
