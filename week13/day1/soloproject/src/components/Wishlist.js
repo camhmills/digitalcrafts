dbb3edaeb2d84aa0ef7d0ae66f4e335b5fd47b1b
Cameron Mills <88257510+camhmills@users.noreply.github.com>
2021-10-26 22:42:59 -0400

    Added a home and wishlist page

---
+++ week13/day1/soloproject/src/components/Wishlist.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { WishlistDiv } from '../styled-components/WishlistStyle'
+
+export default function Wishlist() {
+    return (
+        <WishlistDiv>
+            Wishlist
+        </WishlistDiv>
+    )
+}
---
+++ week13/day1/soloproject/src/styled-components/HomeStyle.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const HomeDiv = styled.div`
+display: grid;
+justify-content: center;
+width: 100vw;
+background-color: #e7eaf6
+`
